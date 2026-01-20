9c41e78c10c23db259b4edfd50f25f8b9634b380
danishRajawat <ibnfarooq070@gmail.com>
2026-01-20 13:19:07 +0530

    experience section done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero.jsx";
 import Navbar from "./layout/Navbar.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
+import Experience from "./sections/Experience.jsx";
 const App = () => {
   return (
     <div className="min-h-screen overflow-x-hidden">
@@ -12,8 +13,8 @@ const App = () => {
         <Hero />
         <About />
         <Projects />
-        {/* <Experience/>
-        <Testimonials/>
+        <Experience/>
+        {/* <Testimonials/>
         <Contact/> */}
       </main>
     </div>
